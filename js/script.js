a0a7134d9347ac1c31fc6b5027f08a85d3dfeeae
Shelbi Mebane <smebane@cristoreyny.org>
2018-05-11 12:28:48 +0000

    javascript

--- js/script.js
+++ js/script.js
@@ -1,32 +1,26 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-$(document).ready(function(){
-  
-  
-  
-  
-});
-
-$.ajax({
-  url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
-  method: "GET",
-  success: function(response) {
-        // Below log the response object to the console
-        console.log(response);
-        //Log the data array to the console
-        console.log(response.data);
-        //Log the first gif objectf from the data array
-        console.log(response.data[0]);
-        //Log the images array to the console
-        console.log(response.data[0].images);
-        //Log the orignal image to the console
-        console.log(response.data[0].images);
-        //Log the url from the orignial gif to the console  
-        console.log(response.data[0].images.fixed_width.url);
+$(document).ready(function() {
+    $("#searchButton").click(function() {
+        var searchTerm;
+        var giphyUrl;
         
-        $('body').append('<img src=' + response.data[0].images.fixed_width.url + '>');
+        searchTerm = $("#searchTerm").val();
         
-  },
+        giphyUrl = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC"; 
+
+        $.ajax({
+          url: giphyUrl,
+          method: "GET",
+          success: function(response) {
+              var imageSrc;
+              var imageHtml;
+              
+              imageHtml= "<img src = " + imageSrc + " >";
+              
+              $(document.body).append(imageHtml);
+          },
+        }); 
+    });
 });
